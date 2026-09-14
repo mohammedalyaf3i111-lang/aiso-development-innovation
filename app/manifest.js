02c762e4c15b0a203eb669f0acc79df770a75e59
mohammedalyaf3i111-lang <mohammed.alyaf3i111@gmail.com>
2026-09-14 18:53:19 +0300

    إضافة بيان تطبيق الموقع

---
+++ app/manifest.js
@@ -0,0 +1,12 @@
+export default function manifest() {
+  return {
+    name: "AISO Development & Innovation",
+    short_name: "AISO",
+    description: "Applied industrial product development and technology transfer.",
+    start_url: "/en/",
+    display: "standalone",
+    background_color: "#071823",
+    theme_color: "#071823",
+    icons: [{ src: "/favicon.svg", sizes: "any", type: "image/svg+xml" }]
+  };
+}
