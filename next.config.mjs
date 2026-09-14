b41229c1abf95a048041204ffbd650054c020acc
mohammedalyaf3i111-lang <mohammed.alyaf3i111@gmail.com>
2026-09-14 18:55:34 +0300

    تهيئة مسار النشر التجريبي

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isPages = process.env.GITHUB_ACTIONS === "true";
 const nextConfig = {
   output: "export",
+  basePath: isPages ? "/aiso-development-innovation" : "",
+  assetPrefix: isPages ? "/aiso-development-innovation/" : "",
   images: { unoptimized: true },
   trailingSlash: true,
   poweredByHeader: false,
